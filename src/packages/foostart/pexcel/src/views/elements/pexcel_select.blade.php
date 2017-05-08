5117d2a96613b5285a35ae5a221ea0326c4eda72
leanpha <leanpha@phuyen.edu.vn>
2017-05-08 19:06:35 +0700

    fix error categories

--- src/packages/foostart/pexcel/src/views/elements/pexcel_select.blade.php
+++ src/packages/foostart/pexcel/src/views/elements/pexcel_select.blade.php
@@ -1,10 +1,11 @@
 <!-- POST CATEGORY LIST -->
 <div class="form-group">
+    <?php $categories = !empty(@$categories) ? $categories : array(); ?>
     <?php $category_id = $request->get('category_id') ? $request->get('category_id') : @$pexcel->category_id ?>
 
     {!! Form::label('pexcel_category_id', trans('pexcel::pexcel.pexcel_categoty_name').':') !!}
 
-    {!! Form::select('pexcel_category_id', @$categories, $category_id, ['class' => 'form-control']) !!}
+    {!! Form::select('pexcel_category_id', $categories, $category_id, ['class' => 'form-control']) !!}
 
 </div>
 <!-- /END POST CATEGORY LIST -->
