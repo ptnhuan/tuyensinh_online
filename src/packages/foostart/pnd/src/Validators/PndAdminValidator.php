da20e2c7f1ae9b1386a7dcb5c076f1ebace7436d
leanpha <leanpha@phuyen.edu.vn>
2017-05-21 17:23:00 +0700

    c

--- src/packages/foostart/pnd/src/Validators/PndAdminValidator.php
+++ src/packages/foostart/pnd/src/Validators/PndAdminValidator.php
@@ -8,11 +8,33 @@ use Illuminate\Support\MessageBag as MessageBag;
 class PndAdminValidator extends AbstractValidator
 {
     protected static $rules = array(
-        'student_first_name' => 'required',
+         'student_first_name' => 'required',
         'student_last_name' => 'required',
         'student_sex' => 'required',
-       
-        
+        'student_birth_day' => 'required',
+        'student_birth_month' => 'required',
+        'student_birth_year' => 'required',
+        'student_birth_place' => 'required',
+        'school_code' => 'required',
+        'school_district_code' => 'required',
+        'student_class' => 'required',
+        'student_capacity_6' => 'required',
+        'student_conduct_6' => 'required',
+        'student_capacity_7' => 'required',
+        'student_conduct_7' => 'required',
+        'student_capacity_8' => 'required',
+        'student_conduct_8' => 'required',
+        'student_capacity_9' => 'required',
+        'student_conduct_9' => 'required',
+        'student_average' => 'required',
+        'student_average_1' => 'required',
+        'student_average_2' => 'required',
+        'student_graduate' => 'required',
+        'student_score_prior' => 'required',
+        'student_score_prior_comment' => 'required',  
+        'school_code_option_1' => 'required',   
+          'student_user' => 'required',
+        'student_pass' => 'required',
         
         
     );
@@ -68,9 +90,33 @@ class PndAdminValidator extends AbstractValidator
 
     public function messages() {
         self::$messages = [
-            'student_first_name.required' => 'Nhập thông tin họ học sinh.',
-            'student_last_name.required' => 'Nhập tên học sinh.',
-             'student_sex.required' => 'Nhập giới tính',
+            'student_first_name.required' => 'Yêu cầu nhập họ học sinh.',
+            'student_last_name.required' => 'Nhập tên học sinh .',
+            'student_sex.required' => 'Nhập giới tính .',
+            'student_birth_day.required' => 'Nhập ngày sinh .',
+            'student_birth_month.required' => 'Nhập tháng sinh .',
+            'student_birth_year.required' => 'Nhập năm sinh .',
+            'student_birth_place.required' => 'Nhập nơi sinh .',
+            'school_code.required' => 'Chọn trường học THCS .',
+            'school_district_code.required' => 'Chọn huyện .',
+            'student_class.required' => 'Nhập lớp .',
+            'student_capacity_6.required' => ' Chọn Học lực Lớp 6  .',
+            'student_conduct_6.required' => ' Chọn Hạnh kiểm Lớp 6 .',
+            'student_capacity_7.required' => ' Chọn Học lực Lớp 7.',
+            'student_conduct_7.required' => ' Chọn Hạnh kiểm Lớp 7.',
+            'student_capacity_8.required' => 'Chọn Học lực Lớp 8  .',
+            'student_conduct_8.required' => 'Chọn Hạnh kiểm Lớp 8 .',
+            'student_capacity_9.required' => 'Chọn Học lực Lớp 9  .',
+            'student_conduct_9.required' => 'Chọn Hạnh kiểm Lớp 9 .',
+            'student_average.required' => ' Nhập điểm TB Lớp 9 .',
+            'student_average_1.required' => ' Nhập điểm TB Môn Toán .',
+            'student_average_2.required' => ' Nhập điểm TB Môn Văn.',
+            'student_graduate.required' => ' Chọn loại Tốt nghiệp .',
+            'student_score_prior.required' => ' Nhập điểm Ưu tiên-khuyến khích .',
+            'student_score_prior_comment.required' => 'Nhập ghi chú Ưu tiên-khuyến khích.',           
+            'school_code_option_1.required' => ' Chọn trường THPT Nguyện vọng 1 .',
+                'student_user.required' => 'Tên đăng nhập.',
+            'student_pass.required' => 'Nhập mật khẩu đăng nhập .',
       
             
         ];
